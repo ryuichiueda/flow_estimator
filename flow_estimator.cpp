8745597190d9a2369ee06444056b19ff1d97fc5e
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-05-21 14:04:18 +0900

    Implement error handling

--- flow_estimator.cpp
+++ flow_estimator.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct Image {
+	unsigned int width;
+	unsigned int height;
+	vector<uint8_t> data;
+};
 
 int main(int argc, char *argv[]) {
+	Image image_before, image_after;
+
+	if(argc != 3) {
+		cerr << "Invalid args" << endl;
+		return 1;
+	}
+
+	ifstream before(argv[1]);
+	ifstream after(argv[2]);
+	if (not before or not after) {
+		cerr << "Invalid files" << endl;
+		return 1;
+	}
+
 
 	return 0;
 }
