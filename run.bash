698caa386d5971e0bb2ee809087a7d2440f25362
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-07-10 10:46:11 +0900

    Writing

--- run.bash
+++ run.bash
@@ -3,7 +3,8 @@
 g++ -O2 -std=c++17 ./flow_sampling.cpp
 
 fix=./pgm_52_57/fixed.pgm
-from=./pgm_52_57/1717995178.795535617.pgm
-to=./pgm_52_57/1717995178.895143492.pgm
+a=./pgm_52_57/1717995178.795535617.pgm
+b=./pgm_52_57/1717995178.895143492.pgm
+c=./pgm_52_57/1717995178.995259531.pgm
 
-time ./a.out $fix $from $to
+time ./a.out $fix $a $b $c
