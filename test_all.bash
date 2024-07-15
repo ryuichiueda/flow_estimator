2b786e520cce55a6a349479d9ddaa5fda885cc92
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-07-15 13:20:09 +0900

    Add a script

--- test_all.bash
+++ test_all.bash
@@ -1,30 +1,18 @@
 #!/bin/bash -xv
 
 g++ -O2 -std=c++17 ./flow_sampling.cpp
-mkdir ans
+mkdir -p ans
 
 tmp=/tmp/$$
-
-
-ls ./pgm_52_57/??????????.?????????.pgm | sort > $tmp-files
 fix=./pgm_52_57/fixed.pgm
 
-paste $tmp-files <(sed 1,5d $tmp-files) <(sed 1,10d $tmp-files) |
-while read from to verify ; do
-	[ "$verify" == "" ] && continue
-
-	time ./a.out <(cat "$from" |sed 's/#.*//') <(cat "$to" |sed 's/#.*//')  <(cat "$verify" |sed 's/#.*//') "$fix" > estimation.pgm
-	convert "$from" "$to" estimation.pgm out.gif
-	convert -scale 400% -delay 100 out.gif ./ans/estm.$(filename $from).gif
+ls ./pgm_52_57/??????????.?????????.pgm |
+sort                                    |
+tr '\n' ' '                             |
+awk '{for(i=1;i<=NF;i++){for(j=i;j<=NF;j++)printf($j" "); print ""}}' |
+awk 'NF>=10{print $1,$2,$3,$4,$5,$10}'  |
+while read a b c d e ref ; do
+	time ./a.out $fix $a $b $c $d $e > ./ans/estm.$(filename $ref)
+	convert $a $b $c $d $e ./ans/estm.$(filename $ref) ./ans/out.gif
+	convert -scale 400% -delay 100 ./ans/out.gif ./ans/estm.$(filename $ref).gif
 done
-
-#from=./pgm_52_57/1717995207.125090281.pgm
-#to=./pgm_52_57/1717995207.625553676.pgm
-#verify=./pgm_52_57/1717995208.125595213.pgm
-#
-#time ./a.out <(cat "$from" |sed 's/#.*//') <(cat "$to" |sed 's/#.*//')  <(cat "$verify" |sed 's/#.*//') "$fix" > a.pgm
-#
-#convert "$from" "$to" a.pgm out.gif
-#convert -scale 400% -delay 100 out.gif out2.gif
-
-rm -f $tmp-*
