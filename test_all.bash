2d499baa2450d1f2d71a4f2ab27d62096d4841e2
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-07-16 13:01:19 +0900

    Change a command

--- test_all.bash
+++ test_all.bash
@@ -12,7 +12,7 @@ tr '\n' ' '                             |
 awk '{for(i=1;i<=NF;i++){for(j=i;j<=NF;j++)printf($j" "); print ""}}' |
 awk 'NF>=10{print $1,$2,$3,$4,$5,$10}'  |
 while read a b c d e ref ; do
-	time ./a.out $fix $a $b $c $d $e > ./ans/estm.$(filename $ref)
-	convert $a $b $c $d $e ./ans/estm.$(filename $ref) ./ans/out.gif
-	convert -scale 400% -delay 100 ./ans/out.gif ./ans/estm.$(filename $ref).gif
+	time ./a.out $fix $a $b $c $d $e > ./ans/estm.$(basename $ref)
+	convert $a $b $c $d $e ./ans/estm.$(basename $ref) ./ans/out.gif
+	convert -scale 400% -delay 100 ./ans/out.gif ./ans/estm.$(basename $ref).gif
 done
